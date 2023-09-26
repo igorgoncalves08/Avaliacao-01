c0addd39124d98d0225c9befaef25d0d1843cd60
Igor Gonçalves <igor.silva7@alunos.unis.edu.br>
2023-09-25 23:51:49 -0300

    avaliacao

---
+++ app/src/main/java/com/example/navegar/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.navegar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navegar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.icNavegar.setOnClickListener{
+            val navegarSegundaTela = Intent(this, SegundaTela::class.java)
+            startActivity(navegarSegundaTela)
+        }
+
+    }
+}
